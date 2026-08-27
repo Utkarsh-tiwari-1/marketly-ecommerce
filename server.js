9b528d7c664616d14ecbe62d147981277e6ed65a
utkarsh <rajshukla2004@gmail.com>
2026-08-27 13:00:56 +0530

    Make catalog module testable

--- server.js
+++ server.js
@@ -107,8 +107,10 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`Marketly is running at http://localhost:${PORT}`);
-});
-
 module.exports = { app, filterAndSortInventory, parseFilters, products };
+
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`Marketly is running at http://localhost:${PORT}`);
+  });
+}
